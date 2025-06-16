95d0145e5bd4e01d55690709526b8f94a73aba1f
Vikas Singh <vsre94@gmail.com>
2025-06-16 14:47:19 +0530

    Auto-sync: Mon Jun 16 14:47:19 IST 2025

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /Users/vikassingh/credit-card-transaction-analysis
+
+cd /Users/vikassingh/Documents/credit-card-transaction-analysis
+git pull origin main --allow-unrelated-histories --no-rebase
 git add .
 git commit -m "Auto-sync: $(date)"
 git push
